e3e63c484c194ee274ed722f7997aac49fed3917
Bladimir <bladimir.peralta@iescesarmanrique.com>
2023-03-02 08:38:58 +0000

    consulta sencilla terminada

--- bbdd_Empresa/consulta_sencilla.sql
+++ bbdd_Empresa/consulta_sencilla.sql
@@ -7,16 +7,16 @@ select nomde, tidir from Departamentos where tidir = 'F' order by nomde asc;
 select nomem, numem, extel from Empleados where numde = 121 order by nomem asc;
 
 --3
-select nomem as 'Nombre', extel as 'Extension Telefonica' from Empleados where numde = 110 order by extel asc;
+select nomem as Nombre, extel as Extension_Telefonica from Empleados where numde = 110 order by extel asc;
 
 --4
-select comis, nomem, salar from Empleados where numhi = 3 order by comis desc;
+select comis, nomem, salar from Empleados where numhi = 3 order by comis desc, nomem asc;
 
 --5
 select comis, nomem, salar from Empleados where numhi = 3 and comis is NOT NULL order by comis asc;
 
 --6
-select salar, nomem from Empleados where salar > 1200 and salar < 1500 and numhi = 0;
+select salar, nomem from Empleados where salar > 1200 and salar < 1500 and numhi = 0 order by salar desc, nomem;
 
 --7
 select numde from Empleados where salar < 1500 group by numde;
