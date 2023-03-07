f6ae2dc007f8cfa5cc45d181125136503b2112b7
Bladimir <bladimir.peralta@iescesarmanrique.com>
2023-03-07 12:19:58 +0000

    consulta con predicado cuantificado terminado

--- bbdd_Empresa/consulta_predcual.sql
+++ bbdd_Empresa/consulta_predcual.sql
@@ -4,5 +4,11 @@
 --1
 select nomem
 from Empleados
-where salar > (select max(salar) from Empleados where numde = 122)
+where salar > all (select salar from Empleados where numde = 122)
+order by nomem asc;
+
+--2
+select nomem
+from Empleados
+where salar > all (select salar from Empleados where numde = 150)
 order by nomem asc;
