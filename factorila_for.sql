e77c5db6aab723740bd7cacf6d3d9e1b2c6e95a0
Bladimir <bladimir.peralta@iescesarmanrique.com>
2023-04-20 09:23:20 +0100

    factorial for terminado

--- factorila_for.sql
+++ factorila_for.sql
@@ -2,16 +2,17 @@ CREATE DEFINER=`bladimir`@`localhost` PROCEDURE `factorial_for`(numero int)
 BEGIN
     declare resultado int;
     declare i int;
-    
+    declare aux int;
     if numero = 0 or numero = 1 then
 		select resultado = 1;
 	elseif numero > 1 then
 		set i = 1;
+        set aux = numero;
         repeat
-			set resultado = i * numero;
-            set numero = resultado;
+			set aux = i * aux;
+            set resultado = aux;
             set i = i + 1;
-		until i = numero
+		until i >= numero
         end repeat;
         select resultado;
 	end if;
